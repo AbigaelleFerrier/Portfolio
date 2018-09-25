49e853bf6e37b9e542b866cbcc8eabfd2da32a2d
Cirill Ferrier <34445590+FerrierCirill@users.noreply.github.com>
2018-09-25 17:52:29 +0200

    Create

---
+++ php/nav.php
@@ -0,0 +1,25 @@
+
+    <nav class="white nav-extended scrollspy" role="navigation" id="nav">
+
+        <div class="nav-wrapper container">
+            <a id="logo-container" href="#" class="brand-logo">Ferrier Cirill</a>
+            
+           <ul class="right hide-on-med-and-down">
+                <?php include 'sous-nav.php' ?>
+            </ul>
+
+            <ul id="nav-mobile" class="sidenav">
+                <?php include 'sous-nav.php' ?>
+            </ul>
+            <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a> 
+        </div>
+
+        <div class="nav-content container center">
+          <ul class="tabs tabs-transparent">
+            <li class="tab"> <a href="#test1">Test 1</a></li>
+            <li class="tab"> <a href="#test2">Test 2</a></li>
+            <li class="tab"> <a href="#test3">Test 3</a></li>
+            <li class="tab"> <a href="#test4">Test 4</a></li>
+          </ul>
+        </div>
+    </nav>
---
+++ php/pageModal.php
@@ -0,0 +1,71 @@
+<!-- Modal Structure -->
+<div id="modal1" class="modal">
+    <div class="modal-content">
+      	<h2>Modal Header</h2>
+     	
+     	<div class="row">
+     		<div class="col s12">
+     			<img class="materialboxed" height="200" src="https://picsum.photos/1920/1080/?random">
+     		</div>
+
+     		<div class="col s12">
+     			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla ariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+     			
+     		</div>
+     	</div>
+     	
+
+     	<div class="row">
+     		<div class="icon-card-i col s3" style="padding-top: 2em">
+		        <i class="fab fa-html5 fa-2x"></i>
+		        <i class="fab fa-css3  fa-2x"></i>
+	            <i class="fab fa-js    fa-2x"></i>
+			</div>
+     	</div>
+      	
+
+
+    </div>
+
+    <div class="modal-footer">
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+    </div>
+</div>
+
+
+
+
+<!-- Arts Pluriel -->
+
+<div id="ArtsPluriel" class="modal">
+    <div class="modal-content">
+      	<h2>Arts Pluriel</h2>
+     	
+     	<div class="row">
+     		<div class="col s12">
+     			<img class="materialboxed" height="200" src="images/logoArtsPluriel.png">
+     		</div>
+
+     		<div class="col s12">
+     			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla ariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+     			
+     		</div>
+     	</div>
+     	
+
+     	<div class="row">
+     		<div class="icon-card-i col s3" style="padding-top: 2em">
+		        <i class="fab fa-html5 fa-2x"></i>
+		        <i class="fab fa-css3  fa-2x"></i>
+	            <i class="fab fa-js    fa-2x"></i>
+			</div>
+     	</div>
+      	
+
+
+    </div>
+
+    <div class="modal-footer">
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+    </div>
+</div>
